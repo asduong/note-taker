857a8055c902eec785bbbd234e9314ec9ea47f82
Andrew Duong <duongandrews@gmail.com>
2020-12-08 16:06:50 -0500

    Added functionality to remove notes

--- notes.js
+++ notes.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const chalk = require('chalk');
 
 const getNotes = function () {
   return 'Your notes...';
@@ -21,6 +22,20 @@ const addNote = function (title, body) {
   }
 };
 
+const removeNote = function (title) {
+  const notes = loadNotes();
+  const notesToKeep = notes.filter(function (note) {
+    return note.title !== title;
+  });
+
+  if (notes.length > notesToKeep.length) {
+    console.log(chalk.green.inverse('Note removed!'));
+    saveNotes(notesToKeep);
+  } else {
+    console.log(chalk.red.inverse('No note found!'));
+  }
+};
+
 const saveNotes = function (notes) {
   const dataJSON = JSON.stringify(notes);
   fs.writeFileSync('notes.json', dataJSON);
@@ -36,4 +51,4 @@ const loadNotes = function () {
   }
 };
 
-module.exports = { getNotes, addNote };
+module.exports = { getNotes, addNote, removeNote };
