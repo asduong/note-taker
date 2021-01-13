375735751301fcde34a49012a768d4b6b0126dcb
Andrew Duong <duongandrews@gmail.com>
2021-01-13 16:52:47 -0500

    Added the list notes feature

--- notes.js
+++ notes.js
@@ -5,9 +5,9 @@ const getNotes = function () {
   return 'Your notes...';
 };
 
-const addNote = function (title, body) {
+const addNote = (title, body) => {
   const notes = loadNotes();
-  const duplicateNotes = notes.filter(function (note) {
+  const duplicateNotes = notes.filter((note) => {
     return note.title === title;
   });
   if (duplicateNotes.length === 0) {
@@ -16,17 +16,15 @@ const addNote = function (title, body) {
       body,
     });
     saveNotes(notes);
-    console.log('New note added!');
+    console.log(chalk.green.inverse('New note added!'));
   } else {
-    console.log('Note title taken!');
+    console.log(chalk.red.inverse('Note title taken!'));
   }
 };
 
-const removeNote = function (title) {
+const removeNote = (title) => {
   const notes = loadNotes();
-  const notesToKeep = notes.filter(function (note) {
-    return note.title !== title;
-  });
+  const notesToKeep = notes.filter((note) => note.title !== title);
 
   if (notes.length > notesToKeep.length) {
     console.log(chalk.green.inverse('Note removed!'));
@@ -36,12 +34,21 @@ const removeNote = function (title) {
   }
 };
 
-const saveNotes = function (notes) {
+const listNotes = () => {
+  const notes = loadNotes();
+  console.log(chalk.inverse('Your notes'));
+
+  notes.forEach((note) => {
+    console.log(note.title);
+  });
+};
+
+const saveNotes = (notes) => {
   const dataJSON = JSON.stringify(notes);
   fs.writeFileSync('notes.json', dataJSON);
 };
 
-const loadNotes = function () {
+const loadNotes = () => {
   try {
     const dataBuffer = fs.readFileSync('notes.json');
     const dataJSON = dataBuffer.toString();
@@ -51,4 +58,4 @@ const loadNotes = function () {
   }
 };
 
-module.exports = { getNotes, addNote, removeNote };
+module.exports = { getNotes, addNote, removeNote, listNotes };
